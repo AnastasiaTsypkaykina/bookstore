af3f76922a4f2d49f832dabab073f87f602c335b
Anastasia Tsypkaykina <tsypkaykina.anastasia@gmail.com>
2025-08-14 13:47:10 +0200

    made some design and added comments by my bookstore

--- assets/template.js
+++ assets/template.js
@@ -1,19 +1,41 @@
 function getTemplateBookCard(index) {
   return `<h1 id="book-title" class="card-title">${books[index].name}</h1>
             <img class="card-img-top" src=${books[index].link} alt="Card image cap" />
-            <p id="book-price">${books[index].price
+            <p >${books[index].price
               .toString()
               .replace(".", ",")} Euro</p>            
-            <p>Author: 
-                <span id="book-author">${books[index].author}</span>
+            <p class="book-properties">Author: 
+                <span class="properties-content" id="book-author">${books[index].author}</span>
             </p>            
-               <p>Erscheinungsjahr: 
-                <span id="book-year">${books[index].publishedYear}</span>
+               <p class="book-properties">Erscheinungsjahr: 
+                <span class="properties-content" id="book-year">${books[index].publishedYear}</span>
             </p>           
-               <p>Genre: 
-                <span id="book-genre">${books[index].genre}</span>
+               <p class="book-properties">Genre: 
+                <span class="properties-content" id="book-genre">${books[index].genre}</span>
             </p> 
             <hr class=solid>
-            <p id="comments">Kommentare: </p>
+           <div class="comment_container">
+           <span class="book-properties">Kommentare: </span>
+                  <table  >
+                    <tbody id="comment-table${index}" ></tbody>
+                  </table>
+                </div>
+                <div class="comment-input">
+                  <input class="input-comment" id="comment-input${index}" type="text" placeholder="Schreibe einen Kommentar"/>
+                  <button onclick="addComment(${index})" >Send  </button>
+              </div>
+            </div>
+            <hr class=solid>
+            <br>
+          `;
+          
+}
+function commentsAndNamesTemplate(commentName) {
+  return `
+            <tr class="comments-content"> 
+              <td class="comment-name">${commentName.name}:</td>
+              <td class = "comment-text">${commentName.comment}</td>
+            </tr>
           `;
 }
+
--- script.js
+++ script.js
@@ -6,5 +6,27 @@ function render() {
   contentCard.innerHTML = "";
   for (let index = 0; index < books.length; index++) {
     contentCard.innerHTML += getTemplateBookCard(index);
+    let tableRef = document.getElementById("comment-table" + index);
+
+    for (let indexComments = 0; indexComments < books[index].comments.length; indexComments++) {      
+      let commentName = books[index].comments[indexComments];
+      tableRef.innerHTML += commentsAndNamesTemplate(commentName);
+    }
   }
 }
+
+function addComment(indexBooks) {
+ 
+  let input = document.getElementById(`comment-input${indexBooks}`);  
+  let commentText = input.value;  
+  books[indexBooks].comments.push({
+    name: "Bücherwurm7",
+    comment: commentText,
+  });
+  let tbodyRef = document.getElementById(`comment-table${indexBooks}`);
+ 
+  tbodyRef.innerHTML +=  `<tr><td>Bücherwurm7: </td><td>${commentText}</td></tr>`;
+ 
+  input.value = "";
+}
+
