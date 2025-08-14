1842739103a50cab9b170756e4325975a241bea6
Anastasia Tsypkaykina <tsypkaykina.anastasia@gmail.com>
2025-08-14 15:34:44 +0200

    made like function and started responsive

--- assets/template.js
+++ assets/template.js
@@ -1,9 +1,27 @@
-function getTemplateBookCard(index) {
+function getTemplateBookCard(index, redHeart, emptyHeart) {
   return `<h1 id="book-title" class="card-title">${books[index].name}</h1>
-            <img class="card-img-top" src=${books[index].link} alt="Card image cap" />
-            <p >${books[index].price
-              .toString()
-              .replace(".", ",")} Euro</p>            
+            <img class="card-img-top cover-img" src=${books[index].link} alt="Card image cap" />
+            <div class="price_and_likes">
+              <span class="book-properties">${books[index].price
+                .toString()
+                .replace(".", ",")} Euro</span>
+             <div class="likes_and_heart">
+                <p id="likes${index}" class="likes properties-content">${books[index].likes}</p> 
+                <button onclick="changeLiked(${index})" class="heart_button">
+                <img
+                class="red_heart_img${redHeart} like-heart"
+                id="red-heart-img${index}"
+                src="./img/heart_filled.png"
+                
+                />
+                <img
+                class="empty_heart_img${emptyHeart} like-heart"
+                id="empty-heart-img${index}"
+                src="./img/heart.png"
+                alt=""
+              />
+            </div>  
+          </div>           
             <p class="book-properties">Author: 
                 <span class="properties-content" id="book-author">${books[index].author}</span>
             </p>            
--- script.js
+++ script.js
@@ -4,11 +4,15 @@ function init() {
 function render() {
   let contentCard = document.getElementById("card-content");
   contentCard.innerHTML = "";
-  for (let index = 0; index < books.length; index++) {
-    contentCard.innerHTML += getTemplateBookCard(index);
+  for (let index = 0; index < books.length; index++) {    
+    let hearts = showRightLike(index);
+    contentCard.innerHTML +=  getTemplateBookCard(index, hearts.redHeart, hearts.emptyHeart);
     let tableRef = document.getElementById("comment-table" + index);
-
-    for (let indexComments = 0; indexComments < books[index].comments.length; indexComments++) {      
+    for (
+      let indexComments = 0;
+      indexComments < books[index].comments.length;
+      indexComments++
+    ) {
       let commentName = books[index].comments[indexComments];
       tableRef.innerHTML += commentsAndNamesTemplate(commentName);
     }
@@ -16,17 +20,64 @@ function render() {
 }
 
 function addComment(indexBooks) {
- 
-  let input = document.getElementById(`comment-input${indexBooks}`);  
-  let commentText = input.value;  
+  let input = document.getElementById(`comment-input${indexBooks}`);
+  let commentText = input.value;
   books[indexBooks].comments.push({
     name: "Bücherwurm7",
     comment: commentText,
   });
   let tbodyRef = document.getElementById(`comment-table${indexBooks}`);
- 
-  tbodyRef.innerHTML +=  `<tr><td>Bücherwurm7: </td><td>${commentText}</td></tr>`;
- 
+
+  tbodyRef.innerHTML += `<tr><td>Bücherwurm7: </td><td>${commentText}</td></tr>`;
+
   input.value = "";
 }
+function showRightLike(indexBooks) {
+  if (books[indexBooks].liked === true) {
+    return { redHeart: "", emptyHeart: " d_none" };
+  } else {
+    return { redHeart: " d_none", emptyHeart: "" };
+  }
+}
+function changeLiked(indexBooks) {
+  
+  if (books[indexBooks].liked === true) {
+   
+    books[indexBooks].liked = false;
+    minusALike(indexBooks);
+    redHeart(indexBooks);
 
+    
+  } else {
+    books[indexBooks].liked = true;
+    plusALike(indexBooks);
+    emptyHeart(indexBooks);
+  } 
+  
+}
+
+function redHeart(indexBooks) {
+  let red = document.getElementById("red-heart-img" + indexBooks);
+  let empty = document.getElementById("empty-heart-img" + indexBooks);
+  red.classList.add("d_none");
+  empty.classList.remove("d_none");
+  
+}
+
+function emptyHeart(indexBooks) {
+  let empty = document.getElementById("empty-heart-img" + indexBooks);
+  let red = document.getElementById("red-heart-img" + indexBooks);
+  empty.classList.add("d_none");
+  red.classList.remove("d_none");
+  
+}
+function plusALike(indexBooks) {
+  books[indexBooks].likes += 1;
+  let likesRef = document.getElementById("likes" + indexBooks);
+  likesRef.innerText = books[indexBooks].likes;
+}
+function minusALike(indexBooks) {
+  books[indexBooks].likes -= 1;
+  let likesRef = document.getElementById("likes" + indexBooks);
+  likesRef.innerText = books[indexBooks].likes;
+}
