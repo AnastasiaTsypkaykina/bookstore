9b5e1e0bc35ca460b15e6101a2eaee46ca43ba3b
Anastasia Tsypkaykina <tsypkaykina.anastasia@gmail.com>
2025-08-19 13:02:51 +0200

    design changes/responsive

--- assets/template.js
+++ assets/template.js
@@ -5,46 +5,13 @@ function getTemplateBookCard(index, redHeart, emptyHeart, fullFavIcon, emptyFavI
         <div class="card-body">
       
           <h1 id="book-title" class="card-title">${books[index].name}</h1>
-            <img class="card-img-top cover-img" src=${
-              books[index].link
-            } alt="Book image" />
-            <div class="price_and_likes">
-              <span class="book-properties">${books[index].price
-                .toString()
-                .replace(".", ",")} Euro</span>
-             <div class="likes_and_heart">
-                <p id="likes${index}" class="likes properties-content">${
-                books[index].likes
-                    }</p> 
-                <button onclick="changeLiked(${index})" class="heart_button" label="button click">
-                <img
-                class="red_heart_img${redHeart} like-heart"
-                id="red-heart-img${index}"
-                src="./img/heart_filled.png"
-                alt="liked"
-                />
-                <img
-                class="empty_heart_img${emptyHeart} like-heart"
-                id="empty-heart-img${index}"
-                src="./img/heart.png"
-                alt="liked"
-                
-              />
-              <button onclick="changeFavorit(${index})" class="favorit_button"label="button click">
-                <img
-                class="fav_icon_img${fullFavIcon} fav_icon"
-                id="full-fav-icon${index}"
-                src="./img/fav_icon_full.svg"
-                alt="favorite"
-                />
-                <img
-                class="empty_fav_img${emptyFavIcon} fav_icon"
-                id="empty-fav-icon${index}"
-                src="./img/fav_icon_empty.svg"
-                alt="favorite"
-              />
-            </div>  
-          </div>           
+            
+            
+        <div class="container-text-img">
+          <div class="book-image">
+            <img class="card-img-top cover-img" src=${books[index].link} alt="Book image" />
+          </div>
+          <div class="text-eigenschaften">
             <p class="book-properties">Author: 
                 <span class="properties-content" id="book-author">${
                   books[index].author
@@ -60,6 +27,45 @@ function getTemplateBookCard(index, redHeart, emptyHeart, fullFavIcon, emptyFavI
                   books[index].genre
                 }</span>
             </p> 
+          </div>
+        </div>
+        <div class="price_and_likes">
+              <span class="book-properties">${books[index].price
+                .toString()
+                .replace(".", ",")} Euro</span>
+             <div class="likes_and_heart">
+              <button onclick="changeFavorit(${index})" class="favorit_button"label="button click">
+              <img
+                class="fav_icon_img${fullFavIcon} fav_icon"
+                id="full-fav-icon${index}"
+                src="./img/fav_icon_full.svg"
+                alt="favorite"
+                />
+              <img
+                class="empty_fav_img${emptyFavIcon} fav_icon"
+                id="empty-fav-icon${index}"
+                src="./img/fav_icon_empty.svg"
+                alt="favorite"
+              /> </button>
+              <p id="likes${index}" class="likes properties-content">${
+                books[index].likes
+                    }</p> 
+              <button onclick="changeLiked(${index})" class="heart_button" label="button click">
+              <img
+                class="red_heart_img${redHeart} like-heart"
+                id="red-heart-img${index}"
+                src="./img/heart_filled.png"
+                alt="liked"
+                />
+              <img
+                class="empty_heart_img${emptyHeart} like-heart"
+                id="empty-heart-img${index}"
+                src="./img/heart.png"
+                alt="liked"                
+              />
+              
+            </div>  
+          </div>
             <hr class=solid>
            <div class="comment_container">
            <span class="book-properties">Kommentare: </span>
@@ -69,7 +75,7 @@ function getTemplateBookCard(index, redHeart, emptyHeart, fullFavIcon, emptyFavI
                 </div>
                 <div class="comment-input">
                   <input class="input-comment" id="comment-input${index}" type="text" placeholder="Schreibe einen Kommentar"/>
-                  <button onclick="addComment(${index})" >Send  </button>
+                  <button class="send-comment"onclick="addComment(${index})" >Send  </button>
               </div>
             </div>
                         
